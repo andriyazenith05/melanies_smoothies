871b622c5e978e1a024c37a070016e040bcbf60b
Andriya Zenith C P <114827921+andriyazenith05@users.noreply.github.com>
2024-07-24 14:40:12 +0530

    Update streamlit_app.py
    Changes to call the Fruityvice API from our SniS App

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,3 +63,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
+# Call the Fruityvice API from Our SniS App
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
