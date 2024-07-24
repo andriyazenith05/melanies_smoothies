0c2639e23e8f10f69abcc525234d57b9dfd53e91
Andriya Zenith C P <114827921+andriyazenith05@users.noreply.github.com>
2024-07-24 11:00:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -23,7 +22,8 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 # Display the Fruit Options List in Your Streamlit in Snowflake (SiS) App. 
 # If the line session = get_active_session() appears in your code two times, delete one of the lines.
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
